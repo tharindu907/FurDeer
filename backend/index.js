bc8c833ac8b9ae2ba2e8c904f8a544be184348f9
tharindu907 <tsandamalamarathunga@gmail.com>
2024-06-18 17:53:52 +0530

    All APIs are created.

--- backend/index.js
+++ backend/index.js
@@ -185,7 +185,7 @@ app.post('/login',async (req,res)=>{
         if (passCompare) {
             const data = {
                 user:{
-                    ide:user.id
+                    id:user.id
                 }
             }
             const token = jwt.sign(data,'secret_fdeer');
@@ -216,6 +216,49 @@ app.get('/popularinwomen',async (req,res)=>{
     res.send(popular_in_women);
 })
 
+// creating middleware to fetch user
+    const fetchUser = async (req,res,next)=>{
+        const token = req.header('auth-token');
+        if (!token){
+            res.status(401).send({errors:"Please authenticate using valid token"})
+        }
+        else{
+            try {
+                const data = jwt.verify(token,'secret_fdeer');
+                req.user = data.user;
+                next();
+            } catch (error) {
+                res.status(401).send({errors:"Please authenticate using valid token"})
+            }
+        }
+    }
+
+//creating endpoint for adding prodcuts in cartdata
+app.post('/addtocart',fetchUser,async (req,res)=>{
+    console.log("Added",req.bodyitemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added")
+})
+
+// creating endpoint to remove product from cartdata
+app.post('/removefromcart',fetchUser,async (req,res)=>{
+    console.log("removed",req.bodyitemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed")
+
+})
+
+//creating endpoint to get cartdata
+app.post('/getcart',fetchUser,async (req,res)=>{
+    console.log("GetCart");
+    let userData = await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
 
 app.listen(port,(error)=>{
     if (!error){
